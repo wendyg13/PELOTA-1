40531936387f713132caaf37879ba4b4a7a1a923
WendyG <wendy.guillermo@ucuenca.edu.ec>
2025-11-03 16:52:50 -0500

    Pelota con elipse

--- sketch.js
+++ sketch.js
@@ -16,15 +16,18 @@ function setup(){
 
     velx = random (-5, 5);   
     vely = random (-5, 5);
+
+    rectMode(CENTER);
 }
 
 function draw(){
     background(150);
-    posx += velx;
-    posy += vely;
 
-    circle(posx, posy, diam);
+    actualizar();
+    visualizar();
+} 
 
+function actualizar (){
     if(posx > width - rad || posx < rad){
         velx *= -1;
     }
@@ -33,23 +36,18 @@ function draw(){
         vely *= -1;
     }
 
-    
-/**
- * funcion actualizar
- * 
- * Actualiza la posición de la bola y verifica colisiones con los bordes
- */
-
-
-function actualizar () {AbortController
-    if (posx > width - rad || posx < rad){
-        velx *= -1;
-    }
-
-    if (posy > height - rad || posy < rad){
-        vely *= -1;
-    }
+    posx += velx;
+    posy += vely;
 
 }
 
+function visualizar(){
+    fill(100, 0, 105);
+    stroke(100,0,100);
+    strokeWeight(10);
+    
+    circle( posx, posy, diam);
+
+    noFill();
+    ellipse(posx, posy, diam/2, diam*2);
 }
