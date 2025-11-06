58db2cd36d3cf7490a970bc4a889084cc3fa5ef5
WendyG <wendy.guillermo@ucuenca.edu.ec>
2025-11-05 21:56:18 -0500

    Personalicé las pelotas

--- classes/Pelota.js
+++ classes/Pelota.js
@@ -2,7 +2,7 @@ class Pelota{
 
     constructor(){
   
-        this.diam = random(10, 150);
+        this.diam = random(20, 180);
         this.rad = this.diam / 2;
 
         this.posx = random(this.rad, width - this.rad);
@@ -11,7 +11,11 @@ class Pelota{
         this.velx = random (-5, 5);   
         this.vely = random (-5, 5);
         
-        this.nuevocolor = color(random(180,270), random(50, 90), random(245, 300), 100);
+        this.nuevocolor = color(random(0,270), random(50, 90), random(150, 300), 100);
+
+        this.nuevocolorCirculo = color(random(100,200), random(0, 50), random(0, 200), 150);
+
+        this.nuevocolorCirculito = color(random(0,100), random(0, 50), random(0, 100), 300);
     }
 
     actualizar(){
@@ -30,11 +34,19 @@ class Pelota{
     }   
 
     visualizar(){
-
     fill(this.nuevocolor);
-    stroke(120,59,100);
-    strokeWeight(5);
+    stroke(20,59,100, 35);
+    strokeWeight(6);
     circle(this.posx, this.posy, this.diam);
+
+    fill(this.nuevocolorCirculo);
+    stroke(20,59,100);
+    noStroke();
+    circle(this.posx, this.posy, this.diam / 1.5);
+
+    fill(this.nuevocolorCirculito);
+    noStroke();
+    circle(this.posx, this.posy, this.diam / 3);
     }
 
 }
